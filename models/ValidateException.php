490d326252358e4745ab5623abf96c56bfbc8516
cronopio <cfrutos@live.com.ar>
2017-06-07 10:33:33 -0300

    Adapt ValidateException for new version of li3

--- models/ValidateException.php
+++ models/ValidateException.php
@@ -3,21 +3,25 @@ namespace li3_doctrine2\models;
 
 use lithium\data\Entity;
 
-class ValidateException extends \Exception {
+class ValidateException extends \Exception
+{
 	protected $entity;
 	protected $errors;
 
-	public function __construct($entity = null) {
+	public function __construct($entity = null)
+    {
 		if (empty($entity)) {
 			return parent::__construct();
 		}
 
 		if ($entity instanceof Entity) {
 			$this->entity = $entity;
-			$this->errors = $entity->errors();
+			$errors = $entity->errors();
 		} else if (is_array($entity)) {
-			$this->errors = $entity;
+			$errors = $entity;
 		}
+
+        $this->errors = $this->convertToOldStyle($errors);
 
 		$message = is_string($entity) ? $entity : '';
 		if (!empty($this->errors)) {
@@ -27,20 +31,35 @@ class ValidateException extends \Exception {
 		parent::__construct($message);
 	}
 
-	public function getEntity() {
+	private function convertToOldStyle(array $errors)
+    {
+        $oldStyleErrors = [];
+        foreach ($errors as $field => $description)
+        {
+            $oldStyleErrors[$field] = is_array($description) ? array_values($description) : $description;
+        }
+
+        return $oldStyleErrors;
+    }
+
+	public function getEntity()
+    {
 		return $this->entity;
 	}
 
-	public function setEntity($entity) {
+	public function setEntity($entity)
+    {
 		$this->entity = $entity;
 	}
 
-	public function getErrors() {
+	public function getErrors()
+    {
 		return $this->errors;
 	}
 
-	public function setErrors($errors) {
-		$this->errors = $errors;
+	public function setErrors($errors)
+    {
+		$this->errors = $this->convertToOldStyle($errors);
 	}
 }
 ?>
